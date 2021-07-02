653e11bb4ae0bf2ea7923d2fab5dbde383cb24bf
jamie denvir <jwdenvir@gmail.com>
2021-07-02 14:33:03 -0400

    gets examples show page to display correct example object

--- src/views/vocab/ExamplesShow.vue
+++ src/views/vocab/ExamplesShow.vue
@@ -2,6 +2,9 @@
   <div class="examples-show">
     <h1>Examples Show</h1>
     <h2>{{ example.word }}</h2>
+    <h3>YOUR PROMPT: {{ example.prompt }}</h3>
+    <img v-bind:src="example.prompt.image_url" v-bind:alt="example.word" />
+    <h2>{{ example.sentence }}</h2>
   </div>
 </template>
 
@@ -10,15 +13,12 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      // example: {
-      //   sentence: "",
-      //   word: "",
-      // },
+      example: {},
     };
   },
   created: function () {
-    axios.get(`/examples/this.$route.params.id`).then((response) => {
-      console.log("Example object", response);
+    axios.get(`/examples/${this.$route.params.id}`).then((response) => {
+      console.log("Example object", response.data);
       this.example = response.data;
     });
   },
