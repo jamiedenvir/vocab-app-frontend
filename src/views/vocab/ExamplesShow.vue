599cc6403c7c0feaacf171f6bf452f60228d6639
jamie denvir <jwdenvir@gmail.com>
2021-07-05 09:30:21 -0400

    adds edit feature to examples show page (more work needed)

--- src/views/vocab/ExamplesShow.vue
+++ src/views/vocab/ExamplesShow.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="examples-show">
     <h5>(examples show page)</h5>
+
     <h1 id="word">{{ example.word }}</h1>
 
     <h3>YOUR PROMPT:</h3>
@@ -15,6 +16,27 @@
 
     <h3>YOUR SENTENCE:</h3>
     <h2>{{ example.sentence }}</h2>
+
+    <!-- edit example sentence  -->
+
+    <form v-on:submit.prevent="updateExample()">
+      <h1>Edit Example</h1>
+      <ul>
+        <li class="text-danger" v-for="error in errors" v-bind:key="error">
+          {{ error }}
+        </li>
+      </ul>
+      <div class="form-group">
+        <label>Sentence:</label>
+        <input
+          type="text"
+          class="form-control"
+          v-model="editExampleParams.sentence"
+          placeholder="Edit Your Sentence Here"
+        />
+      </div>
+      <input type="submit" class="btn btn-primary" value="Submit" />
+    </form>
   </div>
 </template>
 
@@ -39,6 +61,8 @@ export default {
   data: function () {
     return {
       example: {},
+      editExampleParams: {},
+      errors: [],
     };
   },
   created: function () {
@@ -47,6 +71,18 @@ export default {
       this.example = response.data;
     });
   },
-  methods: {},
+  methods: {
+    updateExample: function () {
+      axios
+        .patch(`/examples/${this.editExampleParams.id}`, this.editExampleParams)
+        .then((response) => {
+          console.log(response.data);
+          this.$router.push(`/examples/${response.data.id}`);
+        })
+        .catch((error) => {
+          this.errors = error.response.data.errors;
+        });
+    },
+  },
 };
 </script>
